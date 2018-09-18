5e047df137bb5e2ba68d2270e7657e51677506a7
Victoria B <viktorija.bourassa@gmail.com>
2018-09-18 23:05:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function shout(string){
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 function whisper(string){
-  return string.toLowerCase();
+  return string.toLowerCase()
 }
 function logShout(string){
-  console.log(string).toUppercase;
+  console.log("string").toUppercase;
 }
 function logWhisper(string){
-  console.log(string).toLowerCase;
+  console.log("string").toLowerCase;
 }
