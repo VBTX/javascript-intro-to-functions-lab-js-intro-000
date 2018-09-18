0e689c9fec801b3cc71de6baeab02c0aad9be291
Victoria B <viktorija.bourassa@gmail.com>
2018-09-18 23:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ function sayHiToGrandma(hello){
     return "YES INDEED!";
   }
   }
-  function sayHiToGrandma(hello){
-    if('hello'==="I love you, Grandma."){
-      return "I love you, too";
-    }
-  }
+  function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+ sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
