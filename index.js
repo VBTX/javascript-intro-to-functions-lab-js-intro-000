c1d1f1ac49cd47dd48759c1ba3917369d5e28d45
Victoria B <viktorija.bourassa@gmail.com>
2018-09-18 23:27:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function whisper(HELLO){
   return 'HELLO'.toLowerCase()
 }
 function logShout(hello){
-  console.log("hello".toUppercase);
+  console.log(hello.toUppercase);
 }
 function logWhisper(HELLO){
-  console.log("HELLO".toLowerCase);
+  console.log(HELLO.toLowerCase);
 }
 function sayHiToGrandma(hello){
   if ('hello'.toLowerCase) {
