25dbec1519ba9ae944956f1c507d93dfed7375d4
Victoria B <viktorija.bourassa@gmail.com>
2018-09-18 23:10:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase) {
-    return "I can't hear you!";
+    return "I can't hear you!"
 }
 function sayHiToGrandma(string){
   if(string.toUpperCase){
-    return "YES INDEED!";
+    return "YES INDEED!"
   }
   function sayHiToGrandma(string){
     if(string="I love you, Grandma."){
-      return "I love you, too";
+      return "I love you, too"
     }
   }
 }
